fa6aa6745bdb3e465c0c95d34c4a8cb7e942163d
Derek Gottlieb <derek.gottlieb@gmail.com>
2021-11-25 07:19:44 -0600

    Include arch in spk filename and build both amd64 and arm packages

--- build.sh
+++ build.sh
@@ -4,20 +4,22 @@ set -e
 
 source config.sh
 
-if [[ ! -f nomad_${NOMAD_VERSION}_linux_amd64.zip ]]; then
-  echo "Downloading nomad ${NOMAD_VERSION}"
-  wget https://releases.hashicorp.com/nomad/${NOMAD_VERSION}/nomad_${NOMAD_VERSION}_linux_amd64.zip
-  unzip nomad_${NOMAD_VERSION}_linux_amd64.zip
-  mv nomad builddir/package/bin
-fi
+for arch in amd64 arm arm64
+do
+  if [[ ! -f nomad_${NOMAD_VERSION}_linux_${arch}.zip ]]; then
+    echo "Downloading nomad ${NOMAD_VERSION} / arch ${arch}"
+    wget https://releases.hashicorp.com/nomad/${NOMAD_VERSION}/nomad_${NOMAD_VERSION}_linux_${arch}.zip
+    unzip -d ${arch} nomad_${NOMAD_VERSION}_linux_${arch}.zip
+    mv ${arch}/nomad builddir/package/bin/
+  fi
 
-echo "Generating INFO file"
-./info.sh ${NOMAD_VERSION}-${BUILD_VERSION} > builddir/INFO
+  echo "Generating INFO file for nomad ${NOMAD_VERSION} / arch ${arch}"
+  ./info.sh ${NOMAD_VERSION}-${BUILD_VERSION} > builddir/INFO
 
-echo "Building SPK"
-cd builddir
-tar -cvzf package.tgz package
-tar -cvf ../nomad_${NOMAD_VERSION}.spk *
-rm package.tgz
-
-echo "SPK available at nomad_${NOMAD_VERSION}.spk"
+  echo "Building SPK for nomad ${NOMAD_VERSION} / arch ${arch}"
+  cd builddir
+  tar -cvzf package.tgz package
+  tar -cvf ../nomad_${NOMAD_VERSION}_${arch}.spk *
+  rm package.tgz
+  cd ..
+done
